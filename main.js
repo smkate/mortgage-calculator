e4f0954f0b6b5ca456d82f8e87881d68b1e9474d
Kate <smirkaterinaspb@mail.ru>
2020-09-20 20:11:03 +0300

    Add adaptive and fix problem with wrong end of gradient

--- main.js
+++ main.js
@@ -1,84 +1,84 @@
 /* Values from text inputs */
-const totalCost = document.getElementById('total-cost'),
-      anInitialFee = document.getElementById('an-initial-fee'),
-      creditTerm = document.getElementById('credit-term');
+const totalCost = document.getElementById("total-cost"),
+  anInitialFee = document.getElementById("an-initial-fee"),
+  creditTerm = document.getElementById("credit-term");
 
 /* Values from range inputs */
-const totalCostRange = document.getElementById('total-cost-range'),
-      anInitialFeeRange = document.getElementById('an-initial-fee-range'),
-      creditTermRange = document.getElementById('credit-term-range');
+const totalCostRange = document.getElementById("total-cost-range"),
+  anInitialFeeRange = document.getElementById("an-initial-fee-range"),
+  creditTermRange = document.getElementById("credit-term-range");
 
 /* Result values */
-const totalAmountOfCredit = document.getElementById('amount-of-credit'),
-      totalMonthlyPayment = document.getElementById('monthly-payment'),
-      totalRecommendedIncome = document.getElementById('recommended-income');
+const totalAmountOfCredit = document.getElementById("amount-of-credit"),
+  totalMonthlyPayment = document.getElementById("monthly-payment"),
+  totalRecommendedIncome = document.getElementById("recommended-income");
 
 /* All range: */
-const inputsRange = document.querySelectorAll('.input-range');
-const inputsNumbers = document.querySelectorAll('.input-number');
+const inputsRange = document.querySelectorAll(".input-range");
+const inputsNumbers = document.querySelectorAll(".input-number");
 
 /* All buttons with шnterest rate: */
-const bankBtns = document.querySelectorAll('.bank');
+const bankBtns = document.querySelectorAll(".bank");
 
 // Mapping value from RANGE to input.VALUE
 const assignValue = () => {
-    totalCost.value = totalCostRange.value;
-    anInitialFee.value = anInitialFeeRange.value;
-    creditTerm.value = creditTermRange.value;
-}
+  totalCost.value = totalCostRange.value;
+  anInitialFee.value = anInitialFeeRange.value;
+  creditTerm.value = creditTermRange.value;
+};
 // Mapping value from input.VALUE to RANGE
 const assignValueNumber = () => {
-    totalCostRange.value = totalCost.value;
-    anInitialFeeRange.value = anInitialFee.value;
-    creditTermRange.value = creditTerm.value;
-}
+  totalCostRange.value = totalCost.value;
+  anInitialFeeRange.value = anInitialFee.value;
+  creditTermRange.value = creditTerm.value;
+};
 
 assignValue();
 assignValueNumber();
 
 const banks = [
-    {
-        name: 'alfa',
-        precents: 8.7   
-    },
-    {
-        name: 'sberbank',
-        precents: 8.4   
-    },
-    {
-        name: 'pochta',
-        precents: 7.9   
-    },
-    {
-        name: 'tinkoff',
-        precents: 9.2   
-    }
+  {
+    name: "alfa",
+    precents: 8.7,
+  },
+  {
+    name: "sberbank",
+    precents: 8.4,
+  },
+  {
+    name: "pochta",
+    precents: 7.9,
+  },
+  {
+    name: "tinkoff",
+    precents: 9.2,
+  },
 ];
 
 let currentPrecent = banks[0].precents;
 
 for (let bank of bankBtns) {
-    bank.addEventListener('click', () => {
-        for (let item of bankBtns) {
-            item.classList.remove('active');
-        }
-        bank.classList.add('active');
-        takeActiveBank(bank);
-    })
+  bank.addEventListener("click", () => {
+    for (let item of bankBtns) {
+      item.classList.remove("active");
+    }
+    bank.classList.add("active");
+    takeActiveBank(bank);
+  });
 }
 
-const takeActiveBank = currentActive => {
-    const dataAttrValue = currentActive.dataset.name;
-    const currentBank = banks.find( bank => bank.name === dataAttrValue);
-    currentPrecent = currentBank.precents;
-    calculation(totalCost.value, anInitialFee.value, creditTerm.value);
+const takeActiveBank = (currentActive) => {
+  const dataAttrValue = currentActive.dataset.name;
+  const currentBank = banks.find((bank) => bank.name === dataAttrValue);
+  currentPrecent = currentBank.precents;
+  calculation(totalCost.value, anInitialFee.value, creditTerm.value);
 };
 
 for (let input of inputsRange) {
-    input.addEventListener('input', () => {
-        assignValue();
-        calculation(totalCost.value, anInitialFee.value, creditTerm.value);
-    })
+  input.addEventListener("input", () => {
+    assignValue();
+    calculation(totalCost.value, anInitialFee.value, creditTerm.value);
+  });
 }
 
 // for (let inputNumber of inputsNumbers) {
@@ -89,7 +89,7 @@ for (let input of inputsRange) {
 // }
 
 const calculation = (totalCost = 0, anInitialFee = 100000, creditTerm = 1) => {
-    /*
+  /*
         MP - Monthly Payment
         LA - Loun Amount
         IR - Interest Rate
@@ -98,73 +98,79 @@ const calculation = (totalCost = 0, anInitialFee = 100000, creditTerm = 1) => {
         MP = (LA + (( LA / 100) * IR / 12) * NM) / NM;
     */
 
-    let monthlyPayment; // Monthly Payment
-    let lounAmount = totalCost - anInitialFee; // Loun Amount
-    let interestRate = currentPrecent; // Interest Rate
-    let numberOfYears = creditTerm; // Numver of Years
-    let numberOfMonths = 12 * numberOfYears; // Numver of Months
+  let monthlyPayment; // Monthly Payment
+  let lounAmount = totalCost - anInitialFee; // Loun Amount
+  let interestRate = currentPrecent; // Interest Rate
+  let numberOfYears = creditTerm; // Numver of Years
+  let numberOfMonths = 12 * numberOfYears; // Numver of Months
 
-    monthlyPayment = (lounAmount + (((lounAmount / 100) * interestRate) / 12 ) * numberOfMonths) / numberOfMonths;
-    const monthlyPaymentArounded = Math.round(monthlyPayment);
-    if (monthlyPaymentArounded < 0) {
-        return false;
-    } else {
-        totalAmountOfCredit.innerHTML = `${lounAmount} $`;
-        totalMonthlyPayment.innerHTML = `${monthlyPaymentArounded} $`;
-        totalRecommendedIncome.innerHTML = `${monthlyPaymentArounded + ((monthlyPaymentArounded / 100) * 35) }$`;
-    }
+  monthlyPayment =
+    (lounAmount + (((lounAmount / 100) * interestRate) / 12) * numberOfMonths) /
+    numberOfMonths;
+  const monthlyPaymentArounded = Math.round(monthlyPayment);
+  if (monthlyPaymentArounded < 0) {
+    return false;
+  } else {
+    totalAmountOfCredit.innerHTML = `${lounAmount} $`;
+    totalMonthlyPayment.innerHTML = `${monthlyPaymentArounded} $`;
+    totalRecommendedIncome.innerHTML = `${
+      monthlyPaymentArounded + (monthlyPaymentArounded / 100) * 35
+    }$`;
+  }
 
-    // Creating a Chart doughnut
-    var ctx = document.getElementById('chart');
-    var myChart = new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: [
-                'Сумма кредита',
-                'Ежемесячный платеж',
-                'Рекомендуемый доход',
-            ],
-            datasets: [{
-                label: 'My First dataset',
-                data: [`${lounAmount}`, `${monthlyPaymentArounded}`, `${monthlyPaymentArounded + ((monthlyPaymentArounded / 100) * 35) }`],
-                backgroundColor: [
-                    'rgb(113, 19, 72)',
-                    'rgb(165, 19, 117)',
-                    'rgb(206, 50, 147)',
-                ],
-                borderWidth: 0
-            }]
-        },
-        options: {
-            cutoutPercentage: 80,
-            legend: {
-                display: false,
-            },
-            // Turn off tooltips at chart
-            tooltips: {
-                enabled: false,
-            }
-        },
-    });
-}
-
-
-// Creating a Chart doughnut
-var ctx = document.getElementById('chart');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
+  // Creating a Chart doughnut
+  var ctx = document.getElementById("chart");
+  var myChart = new Chart(ctx, {
+    type: "doughnut",
     data: {
-        datasets: [{
-            backgroundColor: [
-                'rgb(113, 19, 72)',
-                'rgb(165, 19, 117)',
-                'rgb(206, 50, 147)',
-            ],
-            borderWidth: 0
-        }]
+      labels: ["Сумма кредита", "Ежемесячный платеж", "Рекомендуемый доход"],
+      datasets: [
+        {
+          label: "My First dataset",
+          data: [
+            `${lounAmount}`,
+            `${monthlyPaymentArounded}`,
+            `${monthlyPaymentArounded + (monthlyPaymentArounded / 100) * 35}`,
+          ],
+          backgroundColor: [
+            "rgb(113, 19, 72)",
+            "rgb(165, 19, 117)",
+            "rgb(206, 50, 147)",
+          ],
+          borderWidth: 0,
+        },
+      ],
     },
     options: {
-        cutoutPercentage: 80,
+      cutoutPercentage: 80,
+      legend: {
+        display: false,
+      },
+      // Turn off tooltips at chart
+      tooltips: {
+        enabled: false,
+      },
     },
-});
+  });
+};
 
+// Creating a Chart doughnut
+var ctx = document.getElementById("chart");
+var myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        backgroundColor: [
+          "rgb(113, 19, 72)",
+          "rgb(165, 19, 117)",
+          "rgb(206, 50, 147)",
+        ],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    cutoutPercentage: 80,
+  },
+});
